ec990525f2347fcd42df6a5265b2684027318bf4
Blaise-gustave <uyisengablaise@gmail.com>
2019-07-23 05:49:43 -0800

    i remove returns and 2 space

--- sample.rb
+++ sample.rb
@@ -1,33 +1,30 @@
 class Player
 	def hand(hand)
-	return hand
+	  hand
 	end
-  end
+end
    
-  class Enemy
+class Enemy
 	def hand(hand)
-	return hand
+	  hand
 	end
-  end
+end
    
-  class Janken
+class Janken
 	def pon(player_hand, enemy_hand)
-	result=((player_hand-enemy_hand+3)%3)
-	if(result==2)
-	  return "Win"
-	elsif(result==1)
-	  return "Loss"
-	else
-	  return "Draw" 
+	  result=((player_hand-enemy_hand+3)%3)
+	  if(result==2)
+	      "Win"
+	    elsif(result==1)
+	      "Loss"
+	    else
+	      "Draw" 
+	  end
 	end
-	end
-  end
+end
    
-  while(true) do
-	puts "please enter a number:"
-	puts "0: Goo"
-	puts "1: Choki"
-	puts "2: Par"
+while(true) 
+  puts "please enter a number:","0: Goo","1: Choki","2: Par"
 	number=gets.to_i
 	if(number<0 || number>3)
 	  puts "out of boundary"
@@ -40,14 +37,14 @@ class Player
 	  enemy_hand=enemy.hand(randomly)
 	  janken = Janken.new()
 	  game_result = janken.pon(player_hand,enemy_hand)
-	if game_result=="Win"
-	  puts "The hand of the other party is throb. You are the winner"
-	  break
-	elsif game_result== "Loss"
-	  puts "The hand of the other party is throb. You are the looser"
-	  break
-	else
-	  puts "The hand of the other party is throb. It is a draw"
-	end    
+	  if game_result=="Win"
+	    puts "The hand of the other party is throb. You are the winner"
+	    break
+	  elsif game_result== "Loss"
+	    puts "The hand of the other party is throb. You are the looser"
+	    break
+	  else
+	    puts "The hand of the other party is throb. It is a draw"
+	  end    
 	end
-  end
+end
